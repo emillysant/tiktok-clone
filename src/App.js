833d5f0ec53d5e7216294c2fecefa1aac5cf24bd
Emilly-Technook <emilly@technook.com.br>
2023-04-01 18:01:24 -0300

    aula4 - props

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ function App() {
   return (
     <div className="App">
         <div className='app__videos'>
-          <Video/>
-          <Video/>
-          <Video/>
-          <Video/>
-
+          <Video
+            likes={100}
+            messages={200}
+            shares={300}
+            name="Paulo"
+            description="Brecker o goleiro"
+            music="musica épica"
+            url="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/brecker2.mp4?alt=media&token=b7e3ebf7-89a0-47a0-8a34-66f1176a71e3"
+          />
+          <Video
+            likes={120}
+            messages={220}
+            shares={320}
+            name="Pedro"
+            description="Brid olhando a camera"
+            music="Clap your hands"
+            url="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/brecker2.mp4?alt=media&token=b7e3ebf7-89a0-47a0-8a34-66f1176a71e3"
+          />
         </div>
     </div>
   );
